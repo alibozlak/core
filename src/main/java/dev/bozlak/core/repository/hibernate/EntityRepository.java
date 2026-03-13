1623347459a2edf163ce47bcd408c751ae799b11
Ali Bozlak <bozlak20aliihsan@gmail.com>
2026-03-13 23:37:20 +0300

    Deleted EntityRepository.java and its implementation

--- src/main/java/dev/bozlak/core/repository/hibernate/EntityRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.bozlak.core.repository.hibernate;
-
-import dev.bozlak.core.entity.Entity;
-
-public interface EntityRepository<TEntity extends Entity> {
-
-    boolean save(TEntity entity);
-}
